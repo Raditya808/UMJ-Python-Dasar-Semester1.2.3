0a50901484eb377c404367533dbcfb6c288ae4ce
Raditya808 <mraditradit808@gmail.com>
2025-10-31 17:48:54 +0700

    function versi kampus dalam menghitung luas

---
+++ SEMESTER 3/3.Function input fix versi pak kevin dan membuat function menghitung luas.py
@@ -0,0 +1,38 @@
+# python function input versi kampus bapak kevin 
+# contoh 1.
+
+def test(nama,kelas):
+    print("Nama anda :",nama)
+    print("kelas:",kelas)
+
+    
+nm = input("masukan nama:")
+kls = input("masukan kelas:")
+print("="*50)
+test(nm,kls)
+# function versi kampus 
+
+print("="*50)
+
+
+
+# membuat function dalam bentuk aritmatika 
+# versi bapak kevin
+# menggunakan return mengembalikan nilai variabel 
+# contoh 2.
+def testtungluasgluas(panjang,lebar):
+    testtungluas = panjang * luas 
+    return testtungluas
+    print("Hasil kelipatan : ",testtungluas)
+
+panjang = int(input('Masukan panjang: '))
+luas = int(input('Masukan luas: '))
+print("Luas panjang dan : ",testtungluasgluas(panjang,luas)) # memanggil variabel panjang dan luas 
+
+
+print("="*50)
+
+# Tugas pak kevin membuat input barang dan harga seluruh barang 
+# dan membuat total nya 
+# dan membuat tambah ya atau tidak 
+
