ec1a5cf75f8ce74eced5973640babe8bd1ec8b94
Raditya808 <mraditradit808@gmail.com>
2025-11-19 20:24:05 +0700

    function array fix dengan pemanggilan fungsi

--- SEMESTER 3/7.Function input fix versi array.py
+++ SEMESTER 3/7.Function input fix versi array.py
@@ -20,26 +20,30 @@ for i in range(batas):
     harga_untuk_b3 = int(input('Masukan harga belanja ke 3: '))
 
     # menyimpan seluruh data menggunakan data = []
-    data.append((nama, barang1, barang2, barang3, harga_untuk_b1,harga_untuk_b2, harga_untuk_b3))
+    data.append((nama))
 
 
 
 for i in range(batas):
-    data_nama = data[i][0] # mengambil indexing pertama di data append maka yang diambil dulu adalah nama jika angka nya 1 dll maka akan mengambil data yang lain / dan tentunya harus menggunakan data[i][0] agar dapat dipanggil
-    data_barang1 = data[i][1] # data [1] maka akan mengambil data barang1
-    data_barang2 = data[i][2] # data [2] mengambil data_barang2
-    data_barang3 = data[i][3]
-    hitung_harga = data[i][4] + data[i][5] + data[i][6] # data untuk harga_untuk_b1 , harga_untuk_b2, harga_untuk_b3 yang tidak perlu menggunakan variabel lagi karena sudah termasuk data list kosong
+    data_nama = data[i] 
 
- # data[i] berfungsi untuk mengambil/mengakses elemen data yang berada pada posisi indeks 'i' dalam list 'data'.
-
+# [0]mengambil indexing pertama di data append maka yang diambil dulu adalah nama jika angka nya 1 dll maka akan mengambil data yang lain / dan tentunya harus menggunakan data[i][0] agar dapat dipanggil
+# data[i] berfungsi untuk mengambil/mengakses elemen data yang berada pada posisi indeks 'i' dalam list 'data'.
+# dalam python jika suatu append adalah satu karakter maka jika memakai index [0] maka akan menampilkan suatu karakter
+# mengatasi itu terjadi cukup menggunakan [i] saja
+    data_barang = barang(barang1,barang2,barang3) # pemanggilan function dari barang maka akan dijadikan sebuah list barang tersebut 
+    data_harga = hitung_belanja(harga_untuk_b1,harga_untuk_b2,harga_untuk_b3) 
 # dan karena khasus nya menggunakan for i in range
     print("========Struuk belanja anda========")
     print(f"data_nama ke {i+1} {data_nama}") # menampilkan angka dari 1 sampai angka kesekian dari datanya
-    print(f"data_barang ke {i+1} = {data_barang1}") # {i+1} berfungsi membuat data berurutan
-    print(f"data_barang ke {i+1} = {data_barang2}")
-    print(f"data barang ke {i+1} = {data_barang3}")
-    print(f"harga gabungan dari b1 + b2 + b3 = {hitung_harga}")
+    print(f'data barang {data_barang}')
+    print(f'''
+                Nama 
+              harga untuk barang ke 1 = {barang1} 
+              harga untuk barang ke 2 = {barang2}   
+              harga untuk barang ke 3 = {barang3}
+              total = {data_harga}
+    ''')
     print("="*40)
     
     
